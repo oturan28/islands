76e32716c68c9bec977f2745018be5f10b224388
Osman Turan <turan@anwalt.de>
2024-07-08 22:15:48 +0200

    add counter

--- components/Test.island.vue
+++ components/Test.island.vue
@@ -4,8 +4,10 @@ interface Props {
 }
 defineProps<Props>();
 const { data } = await useFetch("https://rickandmortyapi.com/api/character")
+const counter = ref(0)
 </script>
 <template>
+  <p @click="counter++">öaslkdlösakdölsakdölsad {{ counter }}</p>
   <pre>{{ data }}</pre>
   {{ name }}
 </template>
