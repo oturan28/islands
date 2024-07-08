e4de4cbcae8f6e6456d19996d52c6c0f052c7860
Osman Turan <turan@anwalt.de>
2024-07-08 21:54:50 +0200

    add counter

--- components/Test.vue
+++ components/Test.vue
@@ -3,7 +3,9 @@ interface Props {
   name?: string;
 }
 defineProps<Props>();
+const { data } = useFetch("https://rickandmortyapi.com/api/character")
 </script>
 <template>
+  <pre>{{ data }}</pre>
   {{ name }}
 </template>
